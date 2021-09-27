41a552f439266bde605a76b2c935b634071877e2
Imbadimasa <chalganovdima@gmail.com>
2021-09-26 20:02:01 -0400

    Test of the repository

---
+++ Final Project Introduction to C Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_Project_Introduction_to_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
